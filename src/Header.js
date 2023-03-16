e7db7c9d01d123014de3dbfeab81c8d1c102e314
DARSHON CRUDUP <dlc756@yahoo.com>
2023-03-16 00:34:50 -0400

    lab-class03

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,8 @@ import React from "react";
 
 class Header extends React.Component {
   render() {
-    return <h1>Hello 301d97!</h1>
+    console.log(this.props.heart);
+    return <h1>Horned Beast Collection {this.props.hearts}</h1>
   }
 }
 
--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,22 @@
 import React from "react";
-import Person from './Person';
-import data from './data/data.json';
+import Person from "./Person";
 import './Main.css';
 
 class Main extends React.Component {
   render() {
     return (
       <main>
-        {data.map((person,index) => {
-          return <Person 
+        {this.props.beast.map((person,index) => {
+          return (
+          <Person
           name={person._id} 
           title={person.title} 
           image={person.image_url} key={index}
           description={person.description}
+          addHearts={this.props.addHearts}
+          handleOpenModal={this.props.handleOpenModal}
           />
+        )  
         })}
       </main>
     )
